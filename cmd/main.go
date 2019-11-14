112da722de6fbe08f4f4d098598ff73e2d62124d
max-f-z <f.z.jimaogenius@gmail.com>
2019-11-14 11:57:22 +0800

    64 minimum path sum

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	test := [][]int{[]int{1, 3, 1}, []int{1, 5, 1}, []int{4, 2, 1}}
 
-	fmt.Println(minDistance("horse", "ros"))
+	fmt.Println(minPathSum(test))
 }
---
+++ cmd/minimum_path_sum.go
@@ -0,0 +1,34 @@
+package main
+
+func minPathSum(grid [][]int) int {
+	n := len(grid)
+	m := len(grid[0])
+
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, m)
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if i == 0 && j == 0 {
+				dp[i][j] = grid[0][0]
+			} else if i == 0 {
+				dp[i][j] = dp[i][j-1] + grid[i][j]
+			} else if j == 0 {
+				dp[i][j] = dp[i-1][j] + grid[i][j]
+			} else {
+				dp[i][j] = minInt(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+			}
+		}
+	}
+
+	return dp[n-1][m-1]
+}
+
+func minInt(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
