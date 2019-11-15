b7ab6eb80c23029ca56daa49d69f7c1eb329e999
max-f-z <f.z.jimaogenius@gmail.com>
2019-11-15 14:13:08 +0800

    338 counting bits

---
+++ cmd/counting_bits.go
@@ -0,0 +1,16 @@
+package main
+
+func countBits(num int) []int {
+	res := make([]int, num+1)
+	res[0] = 0
+
+	for i := 1; i < num+1; i++ {
+		if i&1 == 0 {
+			res[i] = res[i>>1]
+		} else {
+			res[i] = res[i-1] + 1
+		}
+	}
+
+	return res
+}
--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,5 @@ package main
 import "fmt"
 
 func main() {
-
-	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+	fmt.Println(countBits(10))
 }
