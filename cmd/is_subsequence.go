97ab9f1175e333051d78ffc1e42d2e84266856e9
max-f-z <f.z.jimaogenius@gmail.com>
2019-11-18 10:52:06 +0800

    392 is subsequence

---
+++ cmd/is_subsequence.go
@@ -0,0 +1,22 @@
+package main
+
+func isSubsequence(s string, t string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	index, cnt := 0, 0
+
+	for i := 0; i < len(t); i++ {
+		if t[i] == s[index] {
+			cnt++
+			index++
+		}
+
+		if cnt == len(s) {
+			return true
+		}
+	}
+
+	return false
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println(countBits(10))
+	fmt.Println(isSubsequence("axc", "ahbgdc"))
+
+	data := make(map[string][]string, 3)
+	data["os_username"] = []string{"zhangfuqing"}
+	data["os_password"] = []string{"Polaris123"}
+
+	resp, err := http.PostForm("https://jira.devops.viewchain.net/login.jsp", data)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(resp)
+
 }
