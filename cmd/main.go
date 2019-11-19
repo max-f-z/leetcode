43412e1989b6986aba9001fb2789f8aa351cb7a9
max-f-z <f.z.jimaogenius@gmail.com>
2019-11-19 15:52:31 +0800

    remove unused

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
 func main() {
 	fmt.Println(isSubsequence("axc", "ahbgdc"))
-
-	data := make(map[string][]string, 3)
-	data["os_username"] = []string{"zhangfuqing"}
-	data["os_password"] = []string{"Polaris123"}
-
-	resp, err := http.PostForm("https://jira.devops.viewchain.net/login.jsp", data)
-
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	fmt.Println(resp)
-
 }
