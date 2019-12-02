dee06caa95145e4102c0bf09428fd5904e52604f
max-f-z <f.z.jimaogenius@gmail.com>
2019-12-02 13:42:13 +0800

    139 word break

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(numDecodings("101"))
+	fmt.Println(wordBreak("catsandog", []string{"cats", "dog", "sand", "and", "cat"}))
 }
---
+++ cmd/word_break.go
@@ -0,0 +1,20 @@
+package main
+
+import "strings"
+
+func wordBreak(s string, wordDict []string) bool {
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+
+	for i := 1; i < len(s)+1; i++ {
+		if dp[i-1] == false {
+			continue
+		}
+		for _, v := range wordDict {
+			if t := len(v) + i - 1; t < len(s)+1 && strings.HasPrefix(s[i-1:], v) {
+				dp[t] = true
+			}
+		}
+	}
+	return dp[len(s)]
+}
