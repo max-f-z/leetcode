583e61e8c49a51f54b9233dc5dc46b7fde8f4d83
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-11 17:04:18 +0800

    feat: 3 longest substring without repeating characters

---
+++ cmd/longest_substring_without_repeating_characters.go
@@ -0,0 +1,22 @@
+package main
+
+func lengthOfLongestSubstring(s string) int {
+	max := 0
+	for i := 0; i < len(s); i++ {
+		tmp := 0
+		set := map[string]int{}
+		for j := i; j < len(s); j++ {
+			if set[string(s[j])] == 0 {
+				tmp++
+				set[string(s[j])] = 1
+				continue
+			}
+			break
+		}
+		if tmp > max {
+			max = tmp
+		}
+	}
+
+	return max
+}
--- cmd/main.go
+++ cmd/main.go
@@ -5,24 +5,5 @@ import (
 )
 
 func main() {
-
-	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 0))
-}
-
-func searchInsert(nums []int, target int) int {
-	for i := 0; i < len(nums); i++ {
-		if nums[i] < target {
-			continue
-		}
-
-		if nums[i] == target {
-			return i
-		}
-
-		if nums[i] > target {
-			return i
-		}
-	}
-
-	return len(nums)
+	fmt.Println(lengthOfLongestSubstring("pwwkew"))
 }
