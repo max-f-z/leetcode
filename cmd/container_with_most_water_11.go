3372f301cfcc43a33ca2e505ac7fa24b7cbe4dbd
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-15 16:32:18 +0800

    feat: container with most water 11

---
+++ cmd/container_with_most_water_11.go
@@ -0,0 +1,19 @@
+package main
+
+func maxArea(height []int) int {
+	max := 0
+	for i := 0; i < len(height); i++ {
+		for j := len(height) - 1; j > i; j-- {
+			min := height[j]
+			if height[i] < height[j] {
+				min = height[i]
+			}
+			area := (j - i) * min
+			if area > max {
+				max = area
+			}
+		}
+	}
+
+	return max
+}
