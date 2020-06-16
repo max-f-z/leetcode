51e7624cb1ab555b73faaa905f6f8f9690120015
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-16 20:07:15 +0800

    feat: 19 Remove Nth Node From End of List

---
+++ cmd/19_remove_nth_node_from_end_of_list.go
@@ -0,0 +1,24 @@
+package main
+
+type ListNode19 struct {
+	Val  int
+	Next *ListNode19
+}
+
+func removeNthFromEnd(head *ListNode19, n int) *ListNode19 {
+	dummy := &ListNode19{Next: head}
+	p1, p2 := dummy, dummy
+
+	for i := 0; i <= n; i++ {
+		p1 = p1.Next
+	}
+
+	for p1 != nil {
+		p1 = p1.Next
+		p2 = p2.Next
+	}
+
+	p2.Next = p2.Next.Next
+
+	return dummy.Next
+}
--- cmd/main.go
+++ cmd/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	fmt.Println(fourSum([]int{1, 0, -1, 0, -2, 2}, 0))
+	fmt.Println(removeNthFromEnd(&ListNode19{Val: 1}, 1))
 }
