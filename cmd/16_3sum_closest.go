64316b7eaf11bf818d13092a45857a58c64b0f82
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-16 18:39:49 +0800

    feat: 16 3sum closest

---
+++ cmd/16_3sum_closest.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"math"
+	"sort"
+)
+
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+
+	min := math.MaxInt32
+	res := 0
+
+	for i := 0; i < len(nums)-2; i++ {
+		j := i + 1
+		k := len(nums) - 1
+
+		for j < k {
+			val := nums[i] + nums[j] + nums[k]
+
+			switch {
+			case val == target:
+				return target
+			case val > target:
+				tmp := int(math.Abs(float64(val - target)))
+				if tmp < min {
+					min = tmp
+					res = val
+				}
+				k--
+			case val < target:
+				tmp := int(math.Abs(float64(val - target)))
+				if tmp < min {
+					min = tmp
+					res = val
+				}
+				j++
+			}
+		}
+	}
+	return res
+}
--- cmd/main.go
+++ cmd/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	fmt.Println(threeSum([]int{-1, 0, -1, -1, 0, 1, 2, -1, -4}))
+	fmt.Println(threeSumClosest([]int{-1, 2, 1, -4}, 1))
 }
