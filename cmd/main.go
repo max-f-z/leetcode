f32fd97dd33a2146018cb4161e32f5139feac437
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-18 14:11:14 +0800

    feat: 31 next premutation

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ func main() {
 	l1.Next = &ListNode{Val: 3}
 	l1.Next.Next = &ListNode{Val: 5}
 	l1.Next.Next.Next = &ListNode{Val: 7}
-
-	fmt.Println(findSubstring("wordgoodgoodgoodbestword", []string{"word", "good", "best", "good"}))
+	a := []int{3, 2, 1}
+	nextPermutation(a)
+	fmt.Println(a)
 }
