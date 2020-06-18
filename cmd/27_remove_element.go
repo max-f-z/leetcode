c298b4eaceef11b3908096bb87fd483fff92b9c3
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-18 09:45:41 +0800

    feat: 27 remove element

---
+++ cmd/27_remove_element.go
@@ -0,0 +1,23 @@
+package main
+
+func removeElement(nums []int, val int) int {
+	if len(nums) == 1 {
+		if nums[0] == val {
+			return 0
+		}
+		return len(nums)
+	}
+
+	i, j := 0, 0
+	for j < len(nums) {
+		if nums[j] == val {
+			j++
+		} else {
+			nums[i] = nums[j]
+			i++
+			j++
+		}
+	}
+
+	return i
+}
--- cmd/main.go
+++ cmd/main.go
@@ -10,5 +10,5 @@ func main() {
 	l1.Next.Next = &ListNode{Val: 5}
 	l1.Next.Next.Next = &ListNode{Val: 7}
 
-	fmt.Println(removeDuplicates([]int{1, 1, 2}))
+	fmt.Println(removeElement([]int{3, 2, 2, 3}, 3))
 }
