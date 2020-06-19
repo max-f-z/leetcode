187c4eaaffcb01dbd69f89ff9cab4a910e89a273
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-19 15:07:02 +0800

    feat: 161 one edit distance

---
+++ cmd/161_edit_distance.go
@@ -0,0 +1,32 @@
+package main
+
+func isOneEditDistance(s string, t string) bool {
+	if s == t {
+		return false
+	}
+
+	switch len(s) - len(t) {
+	case 0:
+		for i := 0; i < len(s); i++ {
+			if s[i] != t[i] {
+				return s[i+1:] == t[i+1:]
+			}
+		}
+	case 1:
+		for i := 0; i < len(s)-1; i++ {
+			if s[i] != t[i] {
+				return s[i+1:] == t[i:]
+			}
+		}
+	case -1:
+		for i := 0; i < len(t)-1; i++ {
+			if s[i] != t[i] {
+				return s[i:] == t[i+1:]
+			}
+		}
+	default:
+		return false
+	}
+
+	return true
+}
--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(lengthOfLongestSubstringTwoDistinct("ababffzzeee"))
+	fmt.Println(isOneEditDistance("", ""))
 }
