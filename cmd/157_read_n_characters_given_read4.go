9ef2d4e8409d3e65231da7ba170f131f80e01892
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-19 11:20:15 +0800

    feat: 157 Read N Characters Given Read4

---
+++ cmd/157_read_n_characters_given_read4.go
@@ -0,0 +1,33 @@
+package main
+
+/**
+ * The read4 API is already defined for you.
+ *
+ *     read4 := func(buf []byte) int
+ *
+ * // Below is an example of how the read4 API can be called.
+ * file := File("abcdefghijk") // File is "abcdefghijk", initially file pointer (fp) points to 'a'
+ * buf := make([]byte, 4) // Create buffer with enough space to store characters
+ * read4(buf) // read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+ * read4(buf) // read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+ * read4(buf) // read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+ */
+
+var solution = func(read4 func([]byte) int) func([]byte, int) int {
+	// implement read below.
+	return func(buf []byte, n int) int {
+		count := 0
+		b := make([]byte, 4)
+		for count < n {
+			c := read4(b)
+			for j := 0; j < c && count < n; j++ {
+				buf[count] = b[j]
+				count++
+			}
+			if c < 4 {
+				return count
+			}
+		}
+		return count
+	}
+}
