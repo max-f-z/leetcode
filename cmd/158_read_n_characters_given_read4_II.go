4e265c4139c8e388add258d012fd674109f394e8
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-19 11:37:26 +0800

    feat: 158 Read N Characters Given Read4 II - Call multiple times

---
+++ cmd/158_read_n_characters_given_read4_II.go
@@ -0,0 +1,24 @@
+package main
+
+var solution1 = func(read4 func([]byte) int) func([]byte, int) int {
+	// implement read below.
+	pos := 0
+	size := 0
+	buffer := make([]byte, 4)
+	return func(buf []byte, n int) int {
+		cnt := 0
+		for cnt < n {
+			if size == pos {
+				pos = 0
+				size = read4(buffer)
+				if size == 0 {
+					return cnt
+				}
+			}
+			buf[cnt] = buffer[pos]
+			cnt++
+			pos++
+		}
+		return cnt
+	}
+}
