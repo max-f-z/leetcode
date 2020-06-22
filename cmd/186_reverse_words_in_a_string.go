77c9a43ba65f94f4036f9f79a9b39c973da5ba19
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-22 14:21:28 +0800

    feat: 186 reverse words in a string ||

---
+++ cmd/186_reverse_words_in_a_string.go
@@ -0,0 +1,32 @@
+package main
+
+func reverseWords(s []byte) {
+	l := 0
+	h := len(s) - 1
+	for l < h {
+		s[l], s[h] = s[h], s[l]
+		l++
+		h--
+	}
+
+	start := 0
+	end := 0
+	for i, v := range s {
+		if v != ' ' {
+			end++
+		} else {
+			reverseByte(s, start, end-1)
+			start = i + 1
+			end = start
+		}
+	}
+	reverseByte(s, start, end-1)
+}
+
+func reverseByte(s []byte, l, h int) {
+	for l < h {
+		s[l], s[h] = s[h], s[l]
+		l++
+		h--
+	}
+}
--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	obj := Constructor1()
-	obj.Add(1)
-	obj.Add(3)
-	obj.Add(5)
-
-	fmt.Println(obj.Find(4))
-	fmt.Println(obj.Find(7))
+	b := []byte("the sky is blue")
+	reverseWords(b)
+	fmt.Println(string(b))
 }
