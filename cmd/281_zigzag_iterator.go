c3f3c8d26872cd92a6f1f254e65774ad950d0247
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-29 17:36:45 +0800

    feat: 281. Zigzag Iterator

---
+++ cmd/281_zigzag_iterator.go
@@ -0,0 +1,40 @@
+package main
+
+type ZigzagIterator struct {
+	v1   []int
+	v2   []int
+	flag bool
+}
+
+func Constructor281(v1, v2 []int) *ZigzagIterator {
+	z := &ZigzagIterator{v1: v1, v2: v2, flag: true}
+	return z
+}
+
+func (this *ZigzagIterator) next() int {
+	var v int
+	if this.flag {
+		if len(this.v1) > 0 {
+			v, this.v1 = this.v1[0], this.v1[1:]
+			this.flag = !this.flag
+		} else {
+			v, this.v2 = this.v2[0], this.v2[1:]
+		}
+	} else {
+		if len(this.v2) > 0 {
+			v, this.v2 = this.v2[0], this.v2[1:]
+			this.flag = !this.flag
+		} else {
+			v, this.v1 = this.v1[0], this.v1[1:]
+		}
+	}
+
+	return v
+}
+
+func (this *ZigzagIterator) hasNext() bool {
+	if len(this.v1) == 0 && len(this.v2) == 0 {
+		return false
+	}
+	return true
+}
