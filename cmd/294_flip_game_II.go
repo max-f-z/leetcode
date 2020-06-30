b6e74a9085924cdbd660cd9bbb782f6e92d7c321
max-f-z <f.z.jimaogenius@gmail.com>
2020-06-30 15:27:47 +0800

    feat: 294. Flip Game II

---
+++ cmd/294_flip_game_II.go
@@ -0,0 +1,39 @@
+package main
+
+func canWin(s string) bool {
+	return canWinPlayer1(s)
+}
+
+func canWinPlayer1(s string) bool {
+	for i := 0; i < len(s)-1; i++ {
+		tmp := []byte(s)
+		if s[i] == '+' && s[i+1] == '+' {
+			tmp[i], tmp[i+1] = 45, 45
+			s = string(tmp)
+
+			if !canWinPlayer2(s) {
+				return true
+			}
+			tmp[i], tmp[i+1] = 43, 43
+			s = string(tmp)
+		}
+	}
+	return false
+}
+
+func canWinPlayer2(s string) bool {
+	for i := 0; i < len(s)-1; i++ {
+		tmp := []byte(s)
+		if s[i] == '+' && s[i+1] == '+' {
+			tmp[i], tmp[i+1] = 45, 45
+			s = string(tmp)
+
+			if !canWinPlayer1(s) {
+				return true
+			}
+			tmp[i], tmp[i+1] = 43, 43
+			s = string(tmp)
+		}
+	}
+	return false
+}
