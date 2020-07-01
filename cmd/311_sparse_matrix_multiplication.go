7ed3f01dae818daec49845178559c165889e2e31
max-f-z <f.z.jimaogenius@gmail.com>
2020-07-01 15:44:50 +0800

    feat: 311. Sparse Matrix Multiplication

---
+++ cmd/311_sparse_matrix_multiplication.go
@@ -0,0 +1,21 @@
+package main
+
+func multiply(A [][]int, B [][]int) [][]int {
+	res := make([][]int, len(A))
+
+	if len(B) == 0 {
+		return res
+	}
+	for i := 0; i < len(A); i++ {
+		res[i] = make([]int, len(B[0]))
+		for j := 0; j < len(B[0]); j++ {
+			sum := 0
+			for k := 0; k < len(B); k++ {
+				sum += A[i][k] * B[k][j]
+			}
+			res[i][j] = sum
+		}
+	}
+
+	return res
+}
