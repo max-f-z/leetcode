ee80697e22397be19460851dbbffb9a54c399945
max-f-z <f.z.jimaogenius@gmail.com>
2020-07-07 18:00:58 +0800

    feat: 370. Range Addition

---
+++ cmd/370_range_addition.go
@@ -0,0 +1,22 @@
+package main
+
+func getModifiedArray(length int, updates [][]int) []int {
+	res := make([]int, length)
+
+	for _, v := range updates {
+		start := v[0]
+		end := v[1]
+		update := v[2]
+
+		res[start] += update
+		if end < length-1 {
+			res[end+1] -= update
+		}
+	}
+
+	for i := 1; i < length; i++ {
+		res[i] += res[i-1]
+	}
+
+	return res
+}
