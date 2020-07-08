4a5599e065a8483350e3e19e2bad90ff6d8057c5
max-f-z <f.z.jimaogenius@gmail.com>
2020-07-08 10:13:31 +0800

    feat: 379. Design Phone Directory

---
+++ cmd/379_design_phone_directory.go
@@ -0,0 +1,45 @@
+package main
+
+type PhoneDirectory struct {
+	number []int
+}
+
+func Constructor379(maxNumbers int) PhoneDirectory {
+	var number []int
+	for i := 0; i < maxNumbers; i++ {
+		number = append(number, i)
+	}
+	return PhoneDirectory{number: number}
+}
+
+func (this *PhoneDirectory) Get() int {
+	if len(this.number) == 0 {
+		return -1
+	}
+
+	tmp := this.number[0]
+	this.number = this.number[1:]
+	return tmp
+}
+
+func (this *PhoneDirectory) Check(number int) bool {
+	for _, v := range this.number {
+		if v == number {
+			return true
+		}
+	}
+	return false
+}
+
+func (this *PhoneDirectory) Release(number int) {
+	flag := false
+	for _, v := range this.number {
+		if v == number {
+			flag = true
+		}
+	}
+	if !flag {
+		this.number = append(this.number, number)
+
+	}
+}
