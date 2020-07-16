014dbe3894e1ca43085c5cfd737e64cc28299a6d
max-f-z <f.z.jimaogenius@gmail.com>
2020-07-16 16:00:09 +0800

    feat: 439. Ternary Expression Parser

---
+++ cmd/439_ternary_expression_parser.go
@@ -0,0 +1,43 @@
+package main
+
+import "strings"
+
+func parseTernary(expression string) string {
+	return parseTernaryHelper(expression)
+}
+
+func parseTernaryHelper(expression string) string {
+	idx := strings.LastIndexByte(expression, '?')
+
+	if idx == -1 {
+		return expression
+	}
+
+	nextColonIdx := -1
+	for i := idx + 1; i < len(expression); i++ {
+		if expression[i] == ':' {
+			nextColonIdx = i
+			break
+		}
+	}
+
+	boundry := -1
+	for boundry = nextColonIdx + 1; boundry < len(expression); boundry++ {
+		if expression[boundry] == ':' {
+			break
+		}
+	}
+
+	if expression[idx-1] == 'T' {
+		if boundry == len(expression)-1 {
+			newExpression := expression[:idx-1] + expression[idx+1:nextColonIdx]
+			return parseTernaryHelper(newExpression)
+		}
+		newExpression := expression[:idx-1] + expression[idx+1:nextColonIdx] + expression[boundry:]
+		return parseTernaryHelper(newExpression)
+
+	}
+
+	newExpression := expression[:idx-1] + expression[nextColonIdx+1:]
+	return parseTernaryHelper(newExpression)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -6,19 +6,8 @@ import (
 )
 
 func main() {
-	node5 := &Node431{Val: 5}
-	node6 := &Node431{Val: 6}
-	node3 := &Node431{Val: 3, Children: []*Node431{node5, node6}}
-	node2 := &Node431{Val: 2}
-	node4 := &Node431{Val: 4}
-	root := &Node431{Val: 1, Children: []*Node431{node3, node2, node4}}
-	c := Constructor431()
-	tmp := c.encode(root)
-	fmt.Println(tmp)
-	newNode := c.decode(tmp)
-	fmt.Println(newNode)
-	fmt.Println(tmp)
-
+	// F?1:T?4:5
+	fmt.Println(parseTernary("F?1:T?4:5"))
 	// fmt.Println(wordSquares([]string{"abat", "baba", "atan", "atal"}))
 }
 
