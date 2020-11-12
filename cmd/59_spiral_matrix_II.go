85329d1e135d759d3670a80b4df6863115b7025d
max-f-z <f.z.jimaogenius@gmail.com>
2020-11-12 15:14:12 +0800

    feat: 59 spiral matrix II

---
+++ cmd/59_spiral_matrix_II.go
@@ -0,0 +1,38 @@
+package main
+
+func generateMatrix(n int) [][]int {
+	res := make([][]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = make([]int, n)
+	}
+
+	rowS, rowE, colS, colE := 0, n-1, 0, n-1
+
+	for i := 0; i < n*n; {
+		for idx := colS; i < n*n && idx <= colE; idx++ {
+			res[rowS][idx] = i + 1
+			i++
+		}
+		rowS++
+
+		for idx := rowS; i < n*n && idx <= rowE; idx++ {
+			res[idx][colE] = i + 1
+			i++
+		}
+		colE--
+
+		for idx := colE; i < n*n && idx >= colS; idx-- {
+			res[rowE][idx] = i + 1
+			i++
+		}
+		rowE--
+
+		for idx := rowE; i < n*n && idx >= rowS; idx-- {
+			res[idx][colS] = i + 1
+			i++
+		}
+		colS++
+	}
+
+	return res
+}
