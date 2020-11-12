921dcfda42e7fd0ef168deec1c724594420db99d
max-f-z <f.z.jimaogenius@gmail.com>
2020-11-12 16:27:27 +0800

    feat: 38 count and say

---
+++ cmd/38_count_and_say.go
@@ -0,0 +1,20 @@
+package main
+
+import "strconv"
+
+func countAndSay(n int) string {
+	if n == 1 {
+		return "1"
+	}
+	prev := countAndSay(n - 1)
+	var say string
+	count := 0
+	for i := range prev {
+		count++
+		if i == len(prev)-1 || prev[i] != prev[i+1] {
+			say += strconv.Itoa(count) + prev[i:i+1]
+			count = 0
+		}
+	}
+	return say
+}
