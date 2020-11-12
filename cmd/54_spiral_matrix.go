22d55dd5e936bcc74f8d1cad31e165de98d6a409
max-f-z <f.z.jimaogenius@gmail.com>
2020-11-12 15:13:46 +0800

    feat: 54 spiral matrix

---
+++ cmd/54_spiral_matrix.go
@@ -0,0 +1,43 @@
+package main
+
+func spiralOrder(matrix [][]int) []int {
+	if len(matrix) == 0 {
+		return []int{}
+	}
+
+	m := len(matrix)
+	n := len(matrix[0])
+
+	res := make([]int, m*n)
+
+	rowS, rowE, colS, colE := 0, m-1, 0, n-1
+
+	for i := 0; i < m*n; {
+		for idx := colS; i < len(res) && idx <= colE; idx++ {
+			res[i] = matrix[rowS][idx]
+			i++
+		}
+		rowS++
+
+		for idx := rowS; i < len(res) && idx <= rowE; idx++ {
+			res[i] = matrix[idx][colE]
+			i++
+		}
+		colE--
+
+		for idx := colE; i < len(res) && idx >= colS; idx-- {
+			res[i] = matrix[rowE][idx]
+			i++
+		}
+		rowE--
+
+		for idx := rowE; i < len(res) && idx >= rowS; idx-- {
+			res[i] = matrix[idx][colS]
+			i++
+		}
+		colS++
+
+	}
+
+	return res
+}
