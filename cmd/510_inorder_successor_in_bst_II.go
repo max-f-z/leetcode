48c1c2c27c772592b0a334eb0fef8be28f8e2f2b
max-f-z <f.z.jimaogenius@gmail.com>
2021-02-18 17:04:01 +0800

    feat: leetcode 510 tree

---
+++ cmd/510_inorder_successor_in_bst_II.go
@@ -0,0 +1,35 @@
+package main
+
+// Node510 -
+type Node510 struct {
+	Val    int
+	Left   *Node510
+	Right  *Node510
+	Parent *Node510
+}
+
+func inorderSuccessor510(node *Node510) *Node510 {
+	if node.Right != nil {
+		node = node.Right
+
+		for node.Left != nil {
+			node = node.Left
+		}
+
+		return node
+	} else {
+		p := node
+		successor := new(Node510)
+
+		for p != nil {
+			successor = p.Parent
+			if successor != nil && p == successor.Left {
+				return successor
+			}
+
+			p = successor
+		}
+	}
+
+	return node.Parent
+}
