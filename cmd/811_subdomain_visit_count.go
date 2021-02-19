a8bb8c9d9ce045eb6f11a9729b3d99b1ca7988da
max-f-z <f.z.jimaogenius@gmail.com>
2021-02-19 17:26:07 +0800

    feat: 811. Subdomain Visit Count

---
+++ cmd/811_subdomain_visit_count.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func subdomainVisits(cpdomains []string) []string {
+	tmp := map[string]int{}
+
+	for _, v := range cpdomains {
+		visit := strings.Split(v, " ")
+
+		num, _ := strconv.Atoi(visit[0])
+
+		domains := strings.Split(visit[1], ".")
+
+		for i := len(domains) - 1; i >= 0; i-- {
+			tmp[strings.Join(domains[i:], ".")] += num
+		}
+	}
+
+	result := []string{}
+	for k := range tmp {
+		result = append(result, fmt.Sprintf("%d %s", tmp[k], k))
+	}
+
+	return result
+}
