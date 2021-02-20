34ac49a372705e5ec380a0fa042f48bb44011539
max-f-z <f.z.jimaogenius@gmail.com>
2021-02-20 09:12:21 +0800

    feat: 453. Minimum Moves to Equal Array Elements - math

---
+++ cmd/453_minimum_moves_to_equal_array_elements.go
@@ -0,0 +1,23 @@
+package main
+
+//  sum(all) + m(n-1) = (a1+m) * n
+//  sum(all) + mn -m = a1 * n + mn
+//  m = sum(all) - a1 * n
+func minMoves(nums []int) int {
+	if len(nums) == 1 {
+		return 0
+	}
+
+	sum := 0
+	min := nums[0]
+	n := len(nums)
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+
+		if min > nums[i] {
+			min = nums[i]
+		}
+	}
+
+	return sum - min*n
+}
