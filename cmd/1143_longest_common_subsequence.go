9ee697da02a1dbde69739063a95946bc8fe9a454
max-f-z <f.z.jimaogenius@gmail.com>
2021-02-22 17:19:05 +0800

    feat: 1143. Longest Common Subsequence - DP

---
+++ cmd/1143_longest_common_subsequence.go
@@ -0,0 +1,23 @@
+package main
+
+import "math"
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	l1, l2 := len(text1), len(text2)
+
+	dp := make([][]int, l1+1)
+	for i := 0; i < l1+1; i++ {
+		dp[i] = make([]int, l2+1)
+	}
+	for i := 1; i < l1+1; i++ {
+		for j := 1; j < l2+1; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = int(math.Max(float64(dp[i-1][j]), float64(dp[i][j-1])))
+			}
+		}
+	}
+
+	return dp[l1][l2]
+}
