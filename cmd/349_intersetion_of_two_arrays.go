5bded5654b8d033c7bd776490bb65eea432c5bbe
max-f-z <f.z.jimaogenius@gmail.com>
2021-02-22 09:22:34 +0800

    feat: 349. Intersection of Two Arrays

---
+++ cmd/349_intersetion_of_two_arrays.go
@@ -0,0 +1,23 @@
+package main
+
+func intersection(nums1 []int, nums2 []int) []int {
+	resM := map[int]bool{}
+
+	tmp := map[int]bool{}
+	for i := 0; i < len(nums1); i++ {
+		tmp[nums1[i]] = true
+	}
+
+	for i := 0; i < len(nums2); i++ {
+		if tmp[nums2[i]] {
+			resM[nums2[i]] = true
+		}
+	}
+
+	res := []int{}
+	for k := range resM {
+		res = append(res, k)
+	}
+
+	return res
+}
