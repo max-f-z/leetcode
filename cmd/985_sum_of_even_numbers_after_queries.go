75a6ea30ebc8eaac26dbf97adc28d7f144a3d85a
max-f-z <f.z.jimaogenius@gmail.com>
2021-02-22 10:11:09 +0800

    feat: 985. Sum of Even Numbers After Queries

---
+++ cmd/985_sum_of_even_numbers_after_queries.go
@@ -0,0 +1,31 @@
+package main
+
+func sumEvenAfterQueries(A []int, queries [][]int) []int {
+	sum := 0
+	for i := 0; i < len(A); i++ {
+		if A[i]%2 == 0 {
+			sum += A[i]
+		}
+	}
+
+	res := []int{}
+
+	for i := 0; i < len(queries); i++ {
+		if (A[queries[i][1]]+queries[i][0])%2 == 0 {
+			if A[queries[i][1]]%2 == 0 {
+				sum += queries[i][0]
+			} else {
+				sum += A[queries[i][1]] + queries[i][0]
+			}
+			A[queries[i][1]] = A[queries[i][1]] + queries[i][0]
+		} else {
+			if A[queries[i][1]]%2 == 0 {
+				sum -= A[queries[i][1]]
+			}
+			A[queries[i][1]] = A[queries[i][1]] + queries[i][0]
+		}
+		res = append(res, sum)
+	}
+
+	return res
+}
