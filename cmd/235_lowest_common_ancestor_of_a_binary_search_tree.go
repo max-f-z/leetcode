eada59068ec69170d71170b920320bfacda620c2
max-f-z <f.z.jimaogenius@gmail.com>
2021-03-04 14:49:40 +0800

    feat: 235. Lowest Common Ancestor of a Binary Search Tree

---
+++ cmd/235_lowest_common_ancestor_of_a_binary_search_tree.go
@@ -0,0 +1,29 @@
+package main
+
+func lowestCommonAncestor235(root, p, q *TreeNode) *TreeNode {
+	if root.Val < p.Val && root.Val > q.Val {
+		return root
+	}
+
+	if root.Val > p.Val && root.Val < q.Val {
+		return root
+	}
+
+	if root.Val == p.Val {
+		return root
+	}
+
+	if root.Val == q.Val {
+		return root
+	}
+
+	if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor235(root.Right, p, q)
+	}
+
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor235(root.Left, p, q)
+	}
+
+	return nil
+}
