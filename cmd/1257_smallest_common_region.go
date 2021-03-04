7b523cb5c11f6ab055b5b86fc3b677491b828cb4
max-f-z <f.z.jimaogenius@gmail.com>
2021-03-04 11:27:24 +0800

    feat: 1257. Smallest Common Region - two runners

---
+++ cmd/1257_smallest_common_region.go
@@ -0,0 +1,31 @@
+package main
+
+func findSmallestRegion(regions [][]string, region1 string, region2 string) string {
+	region := map[string]string{}
+
+	for _, r := range regions {
+		for i := 1; i < len(r); i++ {
+			region[r[i]] = r[0]
+		}
+	}
+
+	p := region[region1]
+	q := region[region2]
+
+	for p != q {
+		if p != "" {
+			p = region[p]
+		} else {
+			p = region2
+		}
+
+		if q != "" {
+			q = region[q]
+		} else {
+			q = region1
+		}
+	}
+
+	return p
+
+}
