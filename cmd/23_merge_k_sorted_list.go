54ce55499535b613e607bd0a4dab5ed85f34c144
max-f-z <f.z.jimaogenius@gmail.com>
2021-03-16 17:04:37 +0800

    refactor

--- cmd/23_merge_k_sorted_list.go
+++ cmd/23_merge_k_sorted_list.go
@@ -53,3 +53,67 @@ func (pq *PQ) Pop() (v interface{}) {
 	*pq, v = (*pq)[:pq.Len()-1], (*pq)[pq.Len()-1]
 	return v
 }
+
+type node struct {
+	val int
+	idx int
+	pos int
+}
+
+type mhNode []*node
+
+func (mh mhNode) Len() int {
+	return len(mh)
+}
+
+func (mh mhNode) Less(i, j int) bool {
+	return mh[i].val < mh[j].val
+}
+
+func (mh mhNode) Swap(i, j int) {
+	mh[i], mh[j] = mh[j], mh[i]
+}
+
+func (mh *mhNode) Push(x interface{}) {
+	*mh = append(*mh, x.(*node))
+}
+
+func (mh *mhNode) Pop() (v interface{}) {
+	*mh, v = (*mh)[:mh.Len()-1], (*mh)[mh.Len()-1]
+	return v
+}
+
+func mergeKArrays(lists [][]int) []int {
+	mh := &mhNode{}
+
+	heap.Init(mh)
+
+	for k, v := range lists {
+		if len(v) > 0 {
+			n := &node{
+				val: v[0],
+				idx: k,
+				pos: 0,
+			}
+
+			heap.Push(mh, n)
+		}
+	}
+
+	res := []int{}
+
+	for mh.Len() > 0 {
+		n := heap.Pop(mh).(*node)
+		res = append(res, n.val)
+		if n.pos+1 <= len(lists[n.idx])-1 {
+			nn := &node{
+				val: lists[n.idx][n.pos+1],
+				idx: n.idx,
+				pos: n.pos + 1,
+			}
+			heap.Push(mh, nn)
+		}
+	}
+
+	return res
+}
