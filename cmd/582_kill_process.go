f5487255b9f38ad6b9fe3cba0a67fcf276ccb8dd
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-07 13:26:15 +0800

    feat: 582. Kill Process

---
+++ cmd/582_kill_process.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"container/list"
+)
+
+func killProcess(pid []int, ppid []int, kill int) []int {
+	cols := map[int][]int{}
+
+	for i := 0; i < len(pid); i++ {
+		cols[pid[i]] = []int{}
+	}
+
+	for i := 0; i < len(ppid); i++ {
+		cols[ppid[i]] = append(cols[ppid[i]], pid[i])
+	}
+
+	q := list.New()
+
+	res := []int{}
+
+	q.PushBack(kill)
+	for q.Len() > 0 {
+		val := q.Front()
+		id := val.Value.(int)
+		res = append(res, id)
+		q.Remove(val)
+
+		for _, v := range cols[id] {
+			q.PushBack(v)
+		}
+	}
+
+	return res
+}
