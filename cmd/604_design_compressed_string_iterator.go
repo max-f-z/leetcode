f25b844f7833f3ead884ea15f79a235d46b4d4c1
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-07 14:15:51 +0800

    feat: 604. Design Compressed String Iterator

---
+++ cmd/604_design_compressed_string_iterator.go
@@ -0,0 +1,67 @@
+package main
+
+type StringIterator struct {
+	chars  []byte
+	nums   []int
+	prefix []int
+	idx    int
+}
+
+func Constructor604(compressedString string) StringIterator {
+	chars := []byte{}
+	nums := []int{}
+	prefix := []int{}
+
+	var c byte
+	cnt := 0
+	tmp := 0
+
+	c = compressedString[0]
+
+	for i := 1; i < len(compressedString); i++ {
+		if compressedString[i] >= '0' && compressedString[i] <= '9' {
+			cnt = cnt*10 + int(compressedString[i]-48)
+		} else {
+			prefix = append(prefix, tmp)
+			chars = append(chars, c)
+			nums = append(nums, cnt)
+			tmp += cnt
+			c = compressedString[i]
+			cnt = 0
+		}
+	}
+
+	if cnt != 0 {
+		prefix = append(prefix, tmp)
+		chars = append(chars, c)
+		nums = append(nums, cnt)
+	}
+
+	s := StringIterator{
+		chars:  chars,
+		nums:   nums,
+		prefix: prefix,
+		idx:    0,
+	}
+
+	return s
+}
+
+func (this *StringIterator) Next() byte {
+	if this.idx < this.prefix[len(this.prefix)-1]+this.nums[len(this.nums)-1] {
+		for i := range this.prefix {
+			if this.idx < this.prefix[i]+this.nums[i] {
+				this.idx++
+				return this.chars[i]
+			}
+		}
+	}
+	return ' '
+}
+
+func (this *StringIterator) HasNext() bool {
+	if this.idx < this.prefix[len(this.prefix)-1]+this.nums[len(this.nums)-1] {
+		return true
+	}
+	return false
+}
---
+++ cmd/604_test.go
@@ -0,0 +1,9 @@
+package main
+
+import "testing"
+
+func Test604(t *testing.T) {
+	s := Constructor604("L1e2t1C1o1d1e1")
+
+	t.Log(s)
+}
