487b0f13a731f49f5c29bebf028d3be78b39c72d
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-09 12:58:54 +0800

    feat: 680. Valid Palindrome II

---
+++ cmd/680_valid_panlindrome.go
@@ -0,0 +1,22 @@
+package main
+
+func validPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] == s[len(s)-1-i] {
+			continue
+		}
+
+		return isRangeValid(s, i+1, len(s)-1-i) || isRangeValid(s, i, len(s)-2-i)
+	}
+
+	return true
+}
+
+func isRangeValid(s string, i, j int) bool {
+	for k := i; k <= i+(j-i)/2; k++ {
+		if s[k] != s[j-k+i] {
+			return false
+		}
+	}
+	return true
+}
