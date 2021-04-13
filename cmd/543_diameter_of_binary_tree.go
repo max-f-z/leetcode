f72b79fb3ef3c18431eadc94631762032c74d75f
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-13 17:47:44 +0800

    feat: 543. Diameter of Binary Tree

---
+++ cmd/543_diameter_of_binary_tree.go
@@ -0,0 +1,39 @@
+package main
+
+import "math"
+
+var val int
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	val = math.MinInt32
+
+	tmp := diameterOfBinaryTreeHelper(root)
+
+	if val < tmp {
+		val = tmp
+	}
+
+	return val - 1
+}
+
+func diameterOfBinaryTreeHelper(node *TreeNode) int {
+	if node == nil {
+		return 0
+	}
+
+	if node.Left == nil && node.Right == nil {
+		return 1
+	}
+
+	l := diameterOfBinaryTreeHelper(node.Left)
+	r := diameterOfBinaryTreeHelper(node.Right)
+
+	if val < l+r+1 {
+		val = l + r + 1
+	}
+
+	if l > r {
+		return l + 1
+	}
+	return r + 1
+}
