a11c0da3a448b32ecda0f8852335637583f9fd74
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-13 10:28:45 +0800

    feat: 124. Binary Tree Maximum Path Sum

---
+++ cmd/124_binary_tree_maximum_path_sum.go
@@ -0,0 +1,39 @@
+package main
+
+import "math"
+
+var res int
+
+func maxPathSum(root *TreeNode) int {
+	res = int(math.MinInt32)
+
+	maxPathSumHelper(root)
+
+	return res
+}
+
+func maxPathSumHelper(node *TreeNode) int {
+	if node == nil {
+		return 0
+	}
+
+	l := maxPathSumHelper(node.Left)
+	if l < 0 {
+		l = 0
+	}
+
+	r := maxPathSumHelper(node.Right)
+	if r < 0 {
+		r = 0
+	}
+
+	sum := l + r + node.Val
+	if sum > res {
+		res = sum
+	}
+
+	if l < r {
+		return r + node.Val
+	}
+	return l + node.Val
+}
