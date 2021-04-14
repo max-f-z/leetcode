01753a41e48286b0bc47b366e96af5296b425b05
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-14 11:42:22 +0800

    refactor: refactor 121

--- cmd/best_time_to_buy_and_sell_stock.go
+++ cmd/best_time_to_buy_and_sell_stock.go
@@ -1,13 +1,19 @@
 package main
 
+import "math"
+
 func maxProfit(prices []int) int {
 	max := 0
 
+	min := math.MaxInt32
+
 	for i := 0; i < len(prices); i++ {
-		for j := i + 1; j < len(prices); j++ {
-			if prices[j]-prices[i] > max {
-				max = prices[j] - prices[i]
-			}
+		if min > prices[i] {
+			min = prices[i]
+		}
+
+		if prices[i]-min > max {
+			max = prices[i] - min
 		}
 	}
 
