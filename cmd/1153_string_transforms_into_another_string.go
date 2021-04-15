4d0b5ce48fd8e8818b75e93b60c8de39043ca274
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-15 16:49:58 +0800

    feat: 1153. String Transforms Into Another String

---
+++ cmd/1153_string_transforms_into_another_string.go
@@ -0,0 +1,41 @@
+package main
+
+// str1  a,b,c
+// str2  b,c,a
+
+// step1 a->d d,b,c step2 c->a d,b,a step3 b->c d,c,a step4 d->b b,c,a
+//            b,c,a            b,c,a            b,c,a            b,c,a
+
+func canConvert(str1 string, str2 string) bool {
+	cols := map[byte]byte{}
+
+	if str1 == str2 {
+		return true
+	}
+
+	l := len(str1)
+
+	for i := 0; i < l; i++ {
+		if _, ok := cols[str1[i]]; ok {
+			if cols[str1[i]] == str2[i] {
+				continue
+			}
+			return false
+		}
+
+		if str1[i] == str2[i] {
+			cols[str1[i]] = str2[i]
+			continue
+		}
+
+		cols[str1[i]] = str2[i]
+	}
+
+	buf := map[byte]bool{}
+
+	for i := 0; i < l; i++ {
+		buf[str2[i]] = true
+	}
+
+	return len(buf) != 26
+}
