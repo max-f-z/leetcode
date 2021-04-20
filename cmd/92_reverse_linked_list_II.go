7c3b48bf72f5b16a55656957f8fb951e2f8a2e58
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-20 13:38:21 +0800

    feat: 92. Reverse Linked List II

---
+++ cmd/92_reverse_linked_list_II.go
@@ -0,0 +1,23 @@
+package main
+
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+
+	dummy := &ListNode{}
+	dummy.Next = head
+	prev := dummy
+
+	for i := 1; i < left; i++ {
+		prev = prev.Next
+	}
+
+	cur := prev.Next
+
+	for i := left; i < right; i++ {
+		tmp := cur.Next
+		cur.Next = tmp.Next
+		tmp.Next = prev.Next
+		prev.Next = tmp
+	}
+
+	return dummy.Next
+}
