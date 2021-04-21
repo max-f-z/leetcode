9b5ec8b23b14a275e416ca58d0c46e66ac99c684
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-21 14:07:33 +0800

    feat: 1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts

---
+++ cmd/1465_maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts.go
@@ -0,0 +1,32 @@
+package main
+
+import "sort"
+
+func maxArea1465(h int, w int, horizontalCuts []int, verticalCuts []int) int {
+	sort.Ints(horizontalCuts)
+	sort.Ints(verticalCuts)
+
+	maxH := horizontalCuts[0]
+	maxV := verticalCuts[0]
+	for i := 1; i < len(horizontalCuts); i++ {
+		if maxH < horizontalCuts[i]-horizontalCuts[i-1] {
+			maxH = horizontalCuts[i] - horizontalCuts[i-1]
+		}
+	}
+
+	if maxH < h-horizontalCuts[len(horizontalCuts)-1] {
+		maxH = h - horizontalCuts[len(horizontalCuts)-1]
+	}
+
+	for i := 1; i < len(verticalCuts); i++ {
+		if maxV < verticalCuts[i]-verticalCuts[i-1] {
+			maxV = verticalCuts[i] - verticalCuts[i-1]
+		}
+	}
+
+	if maxV < w-verticalCuts[len(verticalCuts)-1] {
+		maxV = w - verticalCuts[len(verticalCuts)-1]
+	}
+
+	return maxH * maxV % 1000000007
+}
