d4b39830385faa0906ef07941a69a9ed85a3151a
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-27 10:14:44 +0800

    feat: 957. Prison Cells After N Days

---
+++ cmd/957_prison_cells_after_n_days.go
@@ -0,0 +1,34 @@
+package main
+
+func prisonAfterNDays(cells []int, n int) []int {
+	var cur, original [8]int
+
+	for i := 0; i < 8; i++ {
+		cur[i] = cells[i]
+	}
+
+	for i := 0; i < n; i++ {
+		tmp := [8]int{}
+
+		for j := 1; j < 7; j++ {
+			if cur[j-1] == cur[j+1] {
+				tmp[j] = 1
+			}
+		}
+
+		cur = tmp
+
+		if i == 0 {
+			original = cur
+		} else if cur == original {
+			i = (n - 1) / i * i
+		}
+
+	}
+
+	for i := 0; i < 8; i++ {
+		cells[i] = cur[i]
+	}
+
+	return cells
+}
