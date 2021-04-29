bf3768d9b0a77b7541db08e7d822da0bfd872880
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-29 15:12:33 +0800

    feat: 1239. Maximum Length of a Concatenated String with Unique Characters

---
+++ cmd/1239_maximum_length_of_a_concatenated_string_with_unique_characters.go
@@ -0,0 +1,41 @@
+package main
+
+func checkUnique(s string) bool {
+	m := map[byte]int{}
+
+	for i := 0; i < len(s); i++ {
+		m[s[i]]++
+		if m[s[i]] > 1 {
+			return false
+		}
+	}
+	return truef
+}
+
+func maxLength(arr []string) int {
+	return maxLengthHelper(0, arr, "")
+}
+
+func maxLengthHelper(start int, arr []string, prev string) int {
+	if len(prev) > 26 {
+		return 0
+	}
+
+	if start > len(arr)-1 {
+		if checkUnique(prev) {
+			return len(prev)
+		}
+	}
+
+	noCur := maxLengthHelper(start+1, arr, prev)
+	haveCur := 0
+	if checkUnique(prev + arr[start]) {
+		haveCur = maxLengthHelper(start+1, arr, prev+arr[start])
+	}
+
+	if noCur > haveCur {
+		return noCur
+	}
+
+	return haveCur
+}
