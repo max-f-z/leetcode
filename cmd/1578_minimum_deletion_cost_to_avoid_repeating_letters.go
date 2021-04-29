e9218cf02147536855a5248adb8d1821cf8e91ea
max-f-z <f.z.jimaogenius@gmail.com>
2021-04-29 13:44:21 +0800

    feat: 1578. Minimum Deletion Cost to Avoid Repeating Letters

---
+++ cmd/1578_minimum_deletion_cost_to_avoid_repeating_letters.go
@@ -0,0 +1,25 @@
+package main
+
+func minCost1578(s string, cost []int) int {
+	val := 0
+
+	var char byte
+	char = '*'
+	idx := -1
+
+	for i := 0; i < len(s); i++ {
+		if char != s[i] {
+			char = s[i]
+			idx = i
+		} else {
+			if cost[i] > cost[idx] {
+				val += cost[idx]
+				idx = i
+			} else {
+				val += cost[i]
+			}
+		}
+	}
+
+	return val
+}
