bcd75a55e4b6e45050764f5ed1f873a88c3a4153
max-f-z <f.z.jimaogenius@gmail.com>
2021-05-06 18:08:33 +0800

    feat: 141. Linked List Cycle

---
+++ cmd/141_linked_list_cycle.go
@@ -0,0 +1,20 @@
+package main
+
+func hasCycle(head *ListNode) bool {
+	p1, p2 := head, head
+
+	for p1 != nil && p2 != nil {
+		if p2.Next == nil || p2.Next.Next == nil {
+			return false
+		}
+
+		p1 = p1.Next
+		p2 = p2.Next.Next
+
+		if p1 == p2 {
+			return true
+		}
+	}
+
+	return false
+}
