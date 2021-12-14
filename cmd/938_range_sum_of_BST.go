2abc78020081f3029cc55ed45e172696d7f272d0
max-f-z <f.z.polaris89@gmail.com>
2021-12-14 10:47:06 +0800

    feat: 938. Range Sum of BST

---
+++ cmd/938_range_sum_of_BST.go
@@ -0,0 +1,27 @@
+package main
+
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	return rangeSumBSTHelper(root, low, high)
+}
+
+func rangeSumBSTHelper(current *TreeNode, low int, high int) int {
+	if current == nil {
+		return 0
+	}
+
+	sum := 0
+
+	if current.Val >= low && current.Val <= high {
+		sum += current.Val
+	}
+
+	if current.Left != nil {
+		sum += rangeSumBSTHelper(current.Left, low, high)
+	}
+
+	if current.Right != nil {
+		sum += rangeSumBSTHelper(current.Right, low, high)
+	}
+
+	return sum
+}
