19d195bfd1acda0fff87dab5cc2f4f712beeb909
max-f-z <f.z.polaris89@gmail.com>
2021-12-22 09:24:01 +0800

    morris inorder traversal

---
+++ cmd/morris_traversal_inorder.go
@@ -0,0 +1,31 @@
+package main
+
+func morrisInorder(root *TreeNode) []int {
+	ans := []int{}
+
+	current := root
+
+	for current != nil {
+		guide := current.Left
+
+		if guide == nil {
+			ans = append(ans, current.Val)
+			current = current.Right
+		} else {
+			for guide.Right != nil && guide.Right != current {
+				guide = guide.Right
+			}
+
+			if guide.Right == nil {
+				guide.Right = current
+				current = current.Left
+			} else {
+				guide.Right = nil
+				ans = append(ans, current.Val)
+				current = current.Right
+			}
+		}
+	}
+
+	return ans
+}
