5f6a54db15351e8cfaba6a57e448f8ea3f661338
max-f-z <f.z.polaris89@gmail.com>
2021-12-24 10:51:12 +0800

    1137. N-th Tribonacci Number

---
+++ cmd/1137_n-th_tribonacci_number.go
@@ -0,0 +1,22 @@
+package main
+
+func tribonacci(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	if n == 1 || n == 2 {
+		return 1
+	}
+
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 1
+	dp[2] = 1
+
+	for i := 3; i < n+1; i++ {
+		dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+	}
+
+	return dp[n]
+}
