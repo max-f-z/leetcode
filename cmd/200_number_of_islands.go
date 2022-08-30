f11b16a315ce8c35f2f12b5bb384f12a1e35b4b7
max-f-z <f.z.jimaogenius@gmail.com>
2022-08-30 11:20:23 +0800

    200

--- cmd/200_number_of_islands.go
+++ cmd/200_number_of_islands.go
@@ -3,30 +3,29 @@ package main
 func numIslands(grid [][]byte) int {
 	m, n := len(grid), len(grid[0])
 
-	var val byte
-	val = '1'
+	res := 0
 
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			if grid[i][j] == '1' {
-				val++
-				numIslandsHelper(grid, val, i, j, m, n)
+				res++
+				numIslandsHelper(&grid, i, j, m, n)
 			}
 		}
 	}
 
-	return int(val - 1 - 48)
+	return res
 }
 
-func numIslandsHelper(grid [][]byte, val byte, x int, y int, m int, n int) {
-	if x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == '1' {
-		grid[x][y] = val
+func numIslandsHelper(grid *[][]byte, x int, y int, m int, n int) {
+	if x >= 0 && x < m && y >= 0 && y < n && (*grid)[x][y] == '1' {
+		(*grid)[x][y] = '0'
 	} else {
 		return
 	}
-
-	numIslandsHelper(grid, val, x+1, y, m, n)
-	numIslandsHelper(grid, val, x-1, y, m, n)
-	numIslandsHelper(grid, val, x, y+1, m, n)
-	numIslandsHelper(grid, val, x, y-1, m, n)
+	// (*grid)[x][y] = '0'
+	numIslandsHelper(grid, x+1, y, m, n)
+	numIslandsHelper(grid, x-1, y, m, n)
+	numIslandsHelper(grid, x, y+1, m, n)
+	numIslandsHelper(grid, x, y-1, m, n)
 }
