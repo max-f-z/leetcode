4c1d5aeb499676124d18752859bd8445ebcc709d
max-f-z <f.z.jimaogenius@gmail.com>
2022-11-03 09:56:37 +0800

    feat: 766 toeplitz matrix

---
+++ cmd/766_toeplitz_matrix.go
@@ -0,0 +1,15 @@
+package main
+
+func isToeplitzMatrix(matrix [][]int) bool {
+	m, n := len(matrix), len(matrix[0])
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i > 0 && j > 0 && matrix[i-1][j-1] != matrix[i][j] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
