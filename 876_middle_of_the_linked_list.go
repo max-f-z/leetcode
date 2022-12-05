2b894388ab0fe9a56941253a667fd53161532345
max-f-z <f.z.jimaogenius@gmail.com>
2022-12-05 12:20:55 +0800

    feat: 876. Middle of the Linked List

---
+++ 876_middle_of_the_linked_list.go
@@ -0,0 +1,29 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	p1 := head
+	p2 := head
+
+	for p2.Next != nil {
+		p1 = p1.Next
+		p2 = p2.Next
+		if p2.Next != nil {
+			p2 = p2.Next
+		}
+	}
+
+	return p1
+}
