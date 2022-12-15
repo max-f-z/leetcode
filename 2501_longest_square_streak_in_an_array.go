b445d5bd94a8b89e7e587d87949e461d45053299
max-f-z <f.z.jimaogenius@gmail.com>
2022-12-15 18:51:52 +0800

    feat: 2501. Longest Square Streak in an Array

---
+++ 2501_longest_square_streak_in_an_array.go
@@ -0,0 +1,33 @@
+package main
+
+import "sort"
+
+func longestSquareStreak(nums []int) int {
+	vals := map[int]int{}
+	for i := 0; i < len(nums); i++ {
+		vals[nums[i]] = 1
+	}
+
+	sort.Ints(nums)
+
+	max := -1
+
+	cache := map[int]int{}
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		if vals[nums[i]*nums[i]] == 1 {
+			cache[nums[i]] = cache[nums[i]*nums[i]] + 1
+		} else {
+			cache[nums[i]] = 1
+		}
+		if max < cache[nums[i]] {
+			max = cache[nums[i]]
+		}
+	}
+
+	if max == 1 {
+		max = -1
+	}
+
+	return max
+}
