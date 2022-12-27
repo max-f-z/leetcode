03b1257cb8040392abff7ad5287c16f60ef5ed7b
max-f-z <f.z.jimaogenius@gmail.com>
2022-12-27 17:07:23 +0800

    feat: 2279. Maximum Bags With Full Capacity of Rocks

---
+++ cmd/2279_maximum_bags_with_full_capacity_of_rocks.go
@@ -0,0 +1,25 @@
+package main
+
+import "sort"
+
+func maximumBags(capacity []int, rocks []int, additionalRocks int) int {
+	left := make([]int, len(capacity))
+
+	for i := 0; i < len(capacity); i++ {
+		left[i] = capacity[i] - rocks[i]
+	}
+
+	sort.Ints(left)
+
+	ans := 0
+	for i := 0; i < len(left); i++ {
+		if left[i] == 0 {
+			ans++
+		} else if additionalRocks >= left[i] {
+			ans++
+			additionalRocks -= left[i]
+		}
+	}
+
+	return ans
+}
