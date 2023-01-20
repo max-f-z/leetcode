2de33ba99896b9e717008bad0c294ab17064af48
max-f-z <f.z.jimaogenius@gmail.com>
2023-01-20 11:46:37 +0800

    2272. Substring With Largest Variance

---
+++ cmd/2272_substring_with_largest_variance.go
@@ -0,0 +1,51 @@
+package main
+
+func largestVariance(s string) int {
+	max := 0
+	var a, b byte
+	for a = 'a'; a <= 'z'; a++ {
+		for b = 'a'; b <= 'z'; b++ {
+			val := solveOne(a, b, s)
+			if val > max {
+				max = val
+			}
+		}
+	}
+	return max
+}
+
+func solveOne(a, b byte, s string) int {
+	if a == b {
+		return 0
+	}
+
+	max := 0
+	hasB := false
+	bFlag := false
+
+	local := 0
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == a {
+			local++
+		} else if s[i] == b {
+			if bFlag && local >= 0 {
+				bFlag = false
+			} else {
+				local--
+			}
+
+			hasB = true
+
+			if local < 0 {
+				local = -1
+				bFlag = true
+			}
+		}
+
+		if local > max && hasB {
+			max = local
+		}
+	}
+	return max
+}
---
+++ cmd/2272_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func Test2272(t *testing.T) {
+	s := "lripaa"
+	// ans := largestVariance(s)
+
+	ans := solveOne('a', 'l', s)
+
+	t.Log(ans)
+}
