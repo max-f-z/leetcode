fdfbb7c1c0aef8038943b3d2bdba58b29c8f654c
max-f-z <f.z.jimaogenius@gmail.com>
2023-02-06 16:58:37 +0800

    feat: 134. Gas Station

---
+++ cmd/134_gas_station.go
@@ -0,0 +1,25 @@
+package main
+
+func canCompleteCircuit(gas []int, cost []int) int {
+	total := 0
+	current := 0
+
+	start_index := 0
+
+	for i := 0; i < len(gas); i++ {
+		current += gas[i] - cost[i]
+
+		if current < 0 {
+			current = 0
+			start_index = i + 1
+		}
+
+		total += gas[i] - cost[i]
+	}
+
+	if total < 0 {
+		return -1
+	}
+
+	return start_index
+}
