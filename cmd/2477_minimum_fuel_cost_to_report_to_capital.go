0128803e278a4f25ac35de2e957b4b867cdcc2c7
max-f-z <f.z.jimaogenius@gmail.com>
2023-02-14 13:29:28 +0800

    feat: 2477. Minimum Fuel Cost to Report to the Capital

---
+++ cmd/2477_minimum_fuel_cost_to_report_to_capital.go
@@ -0,0 +1,55 @@
+package main
+
+func minimumFuelCost(roads [][]int, seats int) int64 {
+	if len(roads) == 0 {
+		return 0
+	}
+
+	graph := map[int][]int{}
+	for _, v := range roads {
+		if graph[v[0]] == nil {
+			graph[v[0]] = []int{}
+		}
+		if graph[v[1]] == nil {
+			graph[v[1]] = []int{}
+		}
+		graph[v[0]] = append(graph[v[0]], v[1])
+		graph[v[1]] = append(graph[v[1]], v[0])
+	}
+
+	visited := make([]int, len(graph))
+	visited[0] = 1
+	ans, _ := minimumFuelCostDFS(graph, 0, &visited, seats)
+
+	return int64(ans)
+}
+
+func minimumFuelCostDFS(graph map[int][]int, current int, visited *[]int, seats int) (int, int) {
+	reps := 1
+	fuel := 0
+
+	(*visited)[current] = 1
+
+	if current != 0 && len(graph[current]) == 1 {
+		return 1, 1
+	}
+
+	for _, v := range graph[current] {
+		if (*visited)[v] == 0 {
+			(*visited)[v] = 1
+			childFuel, childReps := minimumFuelCostDFS(graph, v, visited, seats)
+			reps += childReps
+			fuel += childFuel
+		}
+	}
+
+	if current != 0 {
+		if reps%seats == 0 {
+			fuel += reps / seats
+		} else {
+			fuel += reps/seats + 1
+		}
+	}
+
+	return fuel, reps
+}
