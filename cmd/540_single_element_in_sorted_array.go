47b6cd857ae8cf1f38828592207801a9b070788a
max-f-z <f.z.jimaogenius@gmail.com>
2023-02-21 11:18:45 +0800

    feat: 540. Single Element in a Sorted Array

---
+++ cmd/540_single_element_in_sorted_array.go
@@ -0,0 +1,31 @@
+package main
+
+func singleNonDuplicate(nums []int) int {
+	l, r := 0, len(nums)-1
+
+	for l < r {
+		mid := (l + r) / 2
+
+		if nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1] {
+			return nums[mid]
+		}
+
+		if nums[mid] == nums[mid-1] {
+			if mid-1 == 0 || (r-mid)%2 == 1 {
+				l = mid + 1
+				continue
+			} else {
+				r = mid - 2
+			}
+		} else {
+			if mid == 0 || (r-mid-1)%2 == 1 {
+				l = mid + 2
+				continue
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+
+	return nums[l]
+}
