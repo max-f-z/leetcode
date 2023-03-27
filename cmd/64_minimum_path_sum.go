15cc525592c7cd15635e29341ad3c82568ee8a2a
max-f-z <f.z.jimaogenius@gmail.com>
2023-03-27 09:57:28 +0800

    feat: 64. Minimum Path Sum

---
+++ cmd/64_minimum_path_sum.go
@@ -0,0 +1,31 @@
+package main
+
+func minPathSumII(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	dp[0][0] = grid[0][0]
+	for i := 1; i < m; i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+
+	for j := 1; j < n; j++ {
+		dp[0][j] = dp[0][j-1] + grid[0][j]
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			min := dp[i-1][j]
+			if dp[i][j-1] < min {
+				min = dp[i][j-1]
+			}
+			dp[i][j] = min + grid[i][j]
+		}
+	}
+
+	return dp[m-1][n-1]
+}
