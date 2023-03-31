119e32631f42df1b36f5a9d63aaa326685b873b6
max-f-z <f.z.jimaogenius@gmail.com>
2023-03-31 18:43:49 +0800

    feat: 90. Subsets II

---
+++ cmd/90_subsets_II.go
@@ -0,0 +1,32 @@
+package main
+
+import "sort"
+
+func subsetsWithDup(nums []int) [][]int {
+	sort.Ints(nums)
+	s := &subset{
+		res:  [][]int{},
+		nums: nums,
+	}
+
+	s.subsetshelperII(0, []int{})
+
+	return s.res
+}
+
+func (s *subset) subsetshelperII(start int, path []int) {
+	resPath := make([]int, len(path))
+	copy(resPath, path)
+	s.res = append(s.res, resPath)
+
+	for i := start; i < len(s.nums); i++ {
+		if i > start && s.nums[i] == s.nums[i-1] {
+			continue
+		}
+		path = append(path, s.nums[i])
+
+		s.subsetshelper(i+1, path)
+
+		path = path[:len(path)-1]
+	}
+}
