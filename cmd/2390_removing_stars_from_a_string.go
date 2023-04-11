28f9a2be5a056d4c2f1722897bbf942685804066
max-f-z <f.z.jimaogenius@gmail.com>
2023-04-11 11:01:00 +0800

    feat: 2390. Removing Stars From a String

---
+++ cmd/2390_removing_stars_from_a_string.go
@@ -0,0 +1,16 @@
+package main
+
+//lint:ignore U1000 unused
+func removeStars(s string) string {
+	var stack []byte
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '*' {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+
+	return string(stack)
+}
