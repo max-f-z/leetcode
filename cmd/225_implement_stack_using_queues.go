a820761a7c496a747a4cc6e7c9415ca64b762177
max-f-z <f.z.jimaogenius@gmail.com>
2023-08-28 15:46:34 +0800

    fix

--- cmd/225_implement_stack_using_queues.go
+++ cmd/225_implement_stack_using_queues.go
@@ -10,20 +10,24 @@ func Constructor225() MyStack {
 	}
 }
 
+//lint:ignore ST1006 this
 func (this *MyStack) Push(x int) {
 	this.elms = append(this.elms, x)
 }
 
+//lint:ignore ST1006 this
 func (this *MyStack) Pop() int {
 	v := this.elms[len(this.elms)-1]
 	this.elms = this.elms[:len(this.elms)-1]
 	return v
 }
 
+//lint:ignore ST1006 this
 func (this *MyStack) Top() int {
 	return this.elms[len(this.elms)-1]
 }
 
+//lint:ignore ST1006 this
 func (this *MyStack) Empty() bool {
 	return len(this.elms) == 0
 }
